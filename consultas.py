5273dfb97bdcafa8d1295cb603b0594b2c8458a4
Sofiadbl <sophia.dbl@puccampinas.edu.br>
2026-04-06 09:23:32 -0300

    Inserção de dados via sql

--- consultas.py
+++ consultas.py
@@ -10,4 +10,28 @@ conexao = mysql.connector.connect(
 
 cursor = conexao.cursor()
 
-# ---- POST - inserir novo usuário ----
+# inserção de dados - MESARIOS
+def inserir_mesarios(cpf, nome, senha):
+    sql = "INSERT INTO mesarios (cpf, nome_completo, login, senha) VALUES (%s, %s, %s, %s)"
+    valores = (cpf, nome, nome+cpf, senha)
+    cursor.execute(sql, valores)
+    conexao.commit()
+    print("Mesário cadastrado com ID: ", cursor.lastrowid)
+
+
+# inserção de dados - USUARIOS 
+def inserir_mesarios(cpf, nome, senha, numero_voto, ja_votou):
+    sql = "INSERT INTO usuarios (cpf, nome_completo, login, senha, numero_voto, ja_votou) VALUES (%s, %s, %s, %s, %s, %s)"
+    valores = (cpf, nome, nome+cpf, senha, numero_voto, ja_votou) #confirmar como salvar a senha com hash
+    cursor.execute(sql, valores)
+    conexao.commit()
+    print("Usuario cadastrado com ID: ", cursor.lastrowid)
+    
+# inserção de dados - CANDIDATOS
+def inserir_candidato(numero, nome, partido):
+    sql = "INSERT INTO candidatos (numero, nome_completo, partido) VALUES (%s, %s, %s)"
+    valores = (numero, nome, partido)
+    cursor.execute(sql, valores)
+    conexao.commit()
+    print("Candidato cadastrado com ID: ", cursor.lastrowid)
+    
